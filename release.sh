dde9df1373f428d190cf0da99b2b63c9d4f94a11
shavyg2 <shavyg2@gmail.com>
2014-04-18 16:17:45 -0400

    automatic publish

--- release.sh
+++ release.sh
@@ -1,4 +1,4 @@
-node_modules/coffee-stir/bin/cli.js -wo src/Application.coffee src/TestCase.coffee
+node_modules/coffee-stir/bin/cli.js -o src/Application.coffee src/TestCase.coffee
 
 
 node_modules/coffee-script/bin/coffee -c -o bin -j beast.testsuite.js src/Application.coffee
@@ -12,3 +12,7 @@ node node_modules/uglifyjs/index.js --source-map bin/beast.testsuite.min.map bin
 
 #Generate documentation
 node_modules/codo/bin/codo -o ./doc src/
+
+
+git add --all
+git commit -am"automatic publish"
