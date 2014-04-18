4810ba46ab7e748f817fa5b37b067d46754daa4b
shavyg2 <shavyg2@gmail.com>
2014-04-18 16:19:31 -0400

    publishing version 0.0.10

--- release.sh
+++ release.sh
@@ -1,8 +1,5 @@
 node_modules/coffee-stir/bin/cli.js -o src/Application.coffee src/TestCase.coffee
-
-
 node_modules/coffee-script/bin/coffee -c -o bin -j beast.testsuite.js src/Application.coffee
-
 
 #compile the test for the application
 node_modules/coffee-script/bin/coffee -c test/test.coffee
@@ -12,7 +9,3 @@ node node_modules/uglifyjs/index.js --source-map bin/beast.testsuite.min.map bin
 
 #Generate documentation
 node_modules/codo/bin/codo -o ./doc src/
-
-
-git add --all
-git commit -am"automatic publish"
